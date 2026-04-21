169878522a0952c9793e2f55d22f26733570c1be
刘泽耀 <1276223841@qq.com>
2026-04-21 09:59:02 +0800

    全能猫狗宠物翻译器    com.cslianta.catdog     v1.0.0.8-O    云珏    oppo    gromore sdk 7.4.2.1  adID:5756629     library-yl-utils-1.0.12 【打回修改】【修改混淆字典，加大垃圾代码 80 80 80 800】【同步最新出包要求】 【com.cslianta.catdog    https://gitee.com/dichenxuan/quan_neng_mao_gou_chong_wu_fan_yi_qi.git 】

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ android {
                 //是否排除生成Activity的Java文件,默认false(layout和写入AndroidManifest.xml还会执行)，主要用于处理类似神策全埋点编译过慢问题
                 otherCountPerPackage = System.getenv("JUNK_OTHER_PER_COUNT")?.toIntOrNull() ?: 50 //每个包下生成其它类的数量
                 methodCountPerClass =  System.getenv("JUNK_OTHER_PER_COUNT")?.toIntOrNull() ?: 50   //每个类下生成方法数量
-                resPrefix = "wtf_"  //生成的layout、drawable、string等资源名前缀
+                resPrefix = "lteg_"  //生成的layout、drawable、string等资源名前缀
                 drawableCount = System.getenv("JUNK_DRAWABLE_COUNT")?.toIntOrNull() ?: 300  //生成drawable资源数量
                 stringCount = System.getenv("JUNK_DRAWABLE_COUNT")?.toIntOrNull() ?: 300 //生成string数量
             }
